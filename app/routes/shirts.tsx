fd8568766eb17420d220298282e30bf83c98b9da
Rodrigo <cuitlagua@gmail.com>
2022-12-19 00:47:48 -0600

    fix: items counters

--- app/routes/shirts.tsx
+++ app/routes/shirts.tsx
@@ -1,6 +1,9 @@
-import { LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { type LoaderFunction, redirect } from '@remix-run/node';
+import { useFetcher, useLoaderData } from '@remix-run/react';
+import type { ActionFunction } from '@remix-run/node';
+
 import NavBar from '~/components/NavBar';
+import { cart } from "~/cookies";
 import ProductCard from '~/components/ProductCard';
 
 export interface ShirtType {
@@ -8,6 +11,8 @@ export interface ShirtType {
   price: string;
   img: string;
   id: number;
+  quantity?: number;
+  totalPrice?: string;
 }
 interface LoaderData {
   shirts: ShirtType[];
@@ -42,8 +47,57 @@ export const loader: LoaderFunction = async () => {
   return { shirts };
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const { product }: any = Object.fromEntries(formData);
+  const cartSession = await cart.getSession(
+    request.headers.get("Cookie")
+  );
+  let items: ShirtType[] = [];
+
+  let parsedProduct: { [k: string]: any } = JSON.parse(product);
+  if (cartSession.has('cart')) {
+    parsedProduct = JSON.parse(product);
+    const cartItems = cartSession.get('cart');
+    const target = parsedProduct["id"];
+
+    const findIndex = cartItems.findIndex((item: string) => {
+      const parsedItem = JSON.parse(item);
+      return parsedItem["id"] === target;
+    });
+
+    if (findIndex > -1) {
+      const parsedItems = JSON.parse(cartItems[findIndex]);
+      cartItems[findIndex] = JSON.stringify({ ...parsedItems, quantity: parsedItems.quantity + 1 });
+      items = [...cartItems];
+    } else {
+      items = [...cartItems, JSON.stringify({ ...parsedProduct, quantity: 1 })];
+    }
+    cartSession.set('cart', items);
+  } else {
+    cartSession.set('cart', [JSON.stringify({ ...parsedProduct, quantity: 1 })]);
+  }
+  const value = await cart.commitSession(cartSession);
+
+  return redirect("/shirts", {
+    headers: {
+      'Set-Cookie': value,
+    },
+  });
+}
+
 export default function Shirts() {
   const { shirts } = useLoaderData<LoaderData>();
+  const fetcher = useFetcher();
+
+  const handleOnClick = (product: {}) => {
+    fetcher.submit(
+      {
+        product: JSON.stringify(product),
+      },
+      { method: 'post' }
+    );
+  }
   return (
     <>
       <NavBar />
@@ -51,7 +105,7 @@ export default function Shirts() {
         <h1 className='text-6xl font-bold mb-12 '>Shirts</h1>
         <article className='flex gap-4 flex-wrap'>
           {shirts.map((shirt) => (
-            <ProductCard product={shirt} key={shirt.id} />
+            <ProductCard product={shirt} key={shirt.id} onClick={handleOnClick} />
           ))}
         </article>
       </section>
